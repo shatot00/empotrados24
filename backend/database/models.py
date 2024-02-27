361f43a747201addb75bd7255787fccc0cc3b4b8
unknown <amm00317@red.ujaen.es>
2024-02-27 13:37:38 +0100

    change database

--- backend/database/models.py
+++ backend/database/models.py
@@ -6,34 +6,39 @@ from .database import Base
 class Compass(Base):
     __tablename__ = "sensors"
 
-    time = Column(String, primary_key=True, nullable=False, index=False)
-    direction = Column(String, primary_key=False, nullable=False, index=False) # N, S, E, W (heading in library)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    time = Column(String, primary_key=False, nullable=False, index=False)
+    direction = Column(float, primary_key=False, nullable=False, index=False) # N, S, E, W (heading in library)
 
 class GPS(Base):
     __tablename__ = "sensors"
 
-    time = Column(String, primary_key=True, nullable=False, index=False)
-    latitude = Column(String, primary_key=False, nullable=False, index=False)
-    longitude = Column(String, primary_key=False, nullable=False, index=False)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    time = Column(String, primary_key=False, nullable=False, index=False)
+    latitude = Column(float, primary_key=False, nullable=False, index=False)
+    longitude = Column(float, primary_key=False, nullable=False, index=False)
 
 class Pedometer(Base):
     __tablename__ = "sensors"
 
-    time = Column(String, primary_key=True, nullable=False, index=False)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    time = Column(String, primary_key=False, nullable=False, index=False)
     steps = Column(int, primary_key=False, nullable=False, index=False)
 
 class Gyroscope(Base):
     __tablename__ = "sensors"
 
-    time = Column(String, primary_key=True, nullable=False, index=False)
-    x = Column(String, primary_key=False, nullable=False, index=False)
-    y = Column(String, primary_key=False, nullable=False, index=False)
-    z = Column(String, primary_key=False, nullable=False, index=False)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    time = Column(String, primary_key=False, nullable=False, index=False)
+    x = Column(float, primary_key=False, nullable=False, index=False)
+    y = Column(float, primary_key=False, nullable=False, index=False)
+    z = Column(float, primary_key=False, nullable=False, index=False)
 
 class Accelerometer(Base):
     __tablename__ = "sensors"
 
-    time = Column(String, primary_key=True, nullable=False, index=False)
-    x = Column(String, primary_key=False, nullable=False, index=False)
-    y = Column(String, primary_key=False, nullable=False, index=False)
-    z = Column(String, primary_key=False, nullable=False, index=False)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    time = Column(String, primary_key=False, nullable=False, index=False)
+    x = Column(float, primary_key=False, nullable=False, index=False)
+    y = Column(float, primary_key=False, nullable=False, index=False)
+    z = Column(float, primary_key=False, nullable=False, index=False)
