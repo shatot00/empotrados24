2259b523258afc7a2bbb32de13273d0e97936387
Luis <lrdf0001@red.ujaen.es>
2024-03-04 12:30:47 +0100

    GPS y Magnetronomo

--- aplicacion/App.js
+++ aplicacion/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Accelerometer, Gyroscope, Pedometer } from 'expo-sensors';
 
+import * as Location from 'expo-location';
+
+import { Magnetometer } from 'expo-sensors';
+
 export default function App() {
   
   const [ dataAccelero, setAccelerometer] = useState({
@@ -16,14 +20,22 @@ export default function App() {
     z: 0,
   });
 
+  const [dataMagneto, setMagneto] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+
   const [subscription, setSubscription] = useState(null);
 
   const _slow = () => { Accelerometer.setUpdateInterval(1000);
-                        Gyroscope.setUpdateInterval(1000); 
+                        Gyroscope.setUpdateInterval(1000);
+                        Magnetometer.setUpdateInterval(1000);
                       }
 
-  const _fast = () => { Accelerometer.setUpdateInterval(16);
-                        Gyroscope.setUpdateInterval(16);
+  const _fast = () => { Accelerometer.setUpdateInterval(200);
+                        Gyroscope.setUpdateInterval(200);
+                        Magnetometer.setUpdateInterval(200);
                       }
 
   const _subscribe = () => {
@@ -33,6 +45,9 @@ export default function App() {
       }), 
       Gyroscope.addListener(gyroscopeData =>{
         setGyroscope(gyroscopeData);
+      }),
+      Magnetometer.addListener(result => {
+        setMagneto(result);
       })
     );
   };
@@ -42,23 +57,55 @@ export default function App() {
     setSubscription(null);
   };
 
+  //--------------------- GPS ---------------------
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
   useEffect(() => {
     _subscribe();
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
     return () => _unsubscribe();
   }, []);
 
+  let textGPS = 'Waiting..';
+
+  if (errorMsg) {
+    textGPS = errorMsg;
+  } else if (location) {
+    textGPS = JSON.stringify(location);
+  }
+ //---------------------------------------------
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Accelerometer: (in gs where 1g = 9.81 m/s^2)</Text>
-      
       <Text style={styles.text}>x: {dataAccelero.x}</Text>
       <Text style={styles.text}>y: {dataAccelero.y}</Text>
       <Text style={styles.text}>z: {dataAccelero.z}</Text>
       
+      <Text style={styles.text}>Giroscopio</Text>
       <Text style={styles.text}>x: {dataGyro.x}</Text>
       <Text style={styles.text}>y: {dataGyro.y}</Text>
-      <Text style={styles.text}>z: {dataGyro.z}</Text>   
+      <Text style={styles.text}>z: {dataGyro.z}</Text>
+
+      <Text style={styles.text}>Magnetometer:</Text>
+      <Text style={styles.text}>x: {dataMagneto.x}</Text>
+      <Text style={styles.text}>y: {dataMagneto.y}</Text>
+      <Text style={styles.text}>z: {dataMagneto.z}</Text>
+
+      <Text style={styles.text}>GPS</Text>
+      <Text style={styles.paragraph}>{textGPS}</Text> 
       
       <View style={styles.buttonContainer}>
         <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
@@ -70,7 +117,7 @@ export default function App() {
         <TouchableOpacity onPress={_fast} style={styles.button}>
           <Text>Fast</Text>
         </TouchableOpacity>
-      </View>
+      </View>      
     </View>
   );
 }
